ca2af861321ca604d96df83b5e1c2ef1dfd99741
Do Chi Hung <dochihungf@gmail.com>
2024-08-22 14:41:23 +0700

    response result asp net core

---
+++ Src/Open.Core/Results/AspNetCore/Exceptions/UnexpectedFailureResultsException.cs
@@ -0,0 +1,16 @@
+﻿namespace Open.Core.Results.AspNetCore.Exceptions;
+
+internal class UnexpectedFailureResultsException : Exception
+{
+    public UnexpectedFailureResultsException(IEnumerable<ResultStatus> statuses)
+    {
+        UnexpectedStatuses = statuses;
+    }
+
+    public IEnumerable<ResultStatus> UnexpectedStatuses { get; }
+
+    public override string ToString()
+    {
+        return $"ActionModel has [{nameof(ExpectedFailuresAttribute)}] with result statuses which are not configured in ResultConvention.";
+    }
+}
---
+++ Src/Open.Core/Results/AspNetCore/ExpectedFailuresAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Open.Core.Results.AspNetCore;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class ExpectedFailuresAttribute : Attribute
+{
+    public ExpectedFailuresAttribute(params ResultStatus[] resultStatuses)
+    {
+        ResultStatuses = resultStatuses;
+    }
+
+    public IEnumerable<ResultStatus> ResultStatuses { get; }
+}
