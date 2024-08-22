6a6457d1b920da45f1d20580d75df90689845a58
Do Chi Hung <dochihungf@gmail.com>
2024-08-22 14:39:01 +0700

    fluent validation response result

---
+++ Src/Open.Core/Results/FluentValidation/FluentValidationResultExtensions.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace Open.Core.Results.FluentValidation;
+
+public static class FluentValidationResultExtensions
+{
+    public static List<ValidationError> AsErrors(this ValidationResult valResult)
+    {
+        var resultErrors = new List<ValidationError>();
+
+        foreach (var valFailure in valResult.Errors)
+        {
+            resultErrors.Add(new ValidationError()
+            {
+                Severity = FromSeverity(valFailure.Severity),
+                ErrorMessage = valFailure.ErrorMessage,
+                ErrorCode = valFailure.ErrorCode,
+                Identifier = valFailure.PropertyName
+            });
+        }
+
+        return resultErrors;
+    }
+
+    public static ValidationSeverity FromSeverity(Severity severity)
+    {
+        switch (severity)
+        {
+            case Severity.Error: return ValidationSeverity.Error;
+            case Severity.Warning: return ValidationSeverity.Warning;
+            case Severity.Info: return ValidationSeverity.Info;
+            default: throw new ArgumentOutOfRangeException(nameof(severity), "Unexpected Severity");
+        }
+    }
+}
