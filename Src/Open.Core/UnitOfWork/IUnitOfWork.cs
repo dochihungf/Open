a5ce7d5f7553bb2c1479bd11f4e3000014e74340
Đo Chi Hung <dochihungf@gmail.com>
2024-08-22 23:02:57 +0700

    unit of work core

---
+++ Src/Open.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace Open.Core.UnitOfWork;
+
+public interface IUnitOfWork : IDisposable
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
+    
+    Task CommitAsync(CancellationToken cancellationToken = new CancellationToken());
+
+    Task RollbackAsync(CancellationToken cancellationToken = new CancellationToken());
+
+    void BeginTransaction();
+}
