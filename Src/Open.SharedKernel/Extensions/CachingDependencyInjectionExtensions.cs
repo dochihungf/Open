0757c1a8a4d445c5c35e955e1844c62d2e347de0
Do Chi Hung <dochihungf@gmail.com>
2024-08-31 21:54:53 +0700

    Update: Add Caching extensions

---
+++ Src/Open.SharedKernel/Extensions/CachingDependencyInjectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Open.Core.GuardClauses;
+using Open.SharedKernel.Caching.Distributed;
+using Open.SharedKernel.Caching.Settings;
+using StackExchange.Redis;
+
+namespace Open.SharedKernel.Extensions;
+
+public static class CachingDependencyInjectionExtensions
+{
+    public static IServiceCollection AddDistributedRedisCaching(this IServiceCollection services, IConfiguration configuration)
+    {
+        var redisCacheSettings = configuration.GetSection(RedisCacheSettings.SectionName).Get<RedisCacheSettings>();
+        Guard.Against.Null(redisCacheSettings, nameof(redisCacheSettings));
+        
+        var asyncPolicy = PollyExtensions.CreateDefaultPolicy(cfg =>
+        {
+            cfg.Or<RedisServerException>()
+                .Or<RedisConnectionException>();
+        });
+        
+        services.AddSingleton<IDistributedRedisCache>(s => new DistributedRedisCache(
+            redisCacheSettings.ConnectionString,
+            redisCacheSettings.InstanceName, 
+            redisCacheSettings.DatabaseIndex,
+            asyncPolicy));
+
+        return services;
+    }
+}
