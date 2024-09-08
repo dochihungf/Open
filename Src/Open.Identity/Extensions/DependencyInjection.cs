b76324b0b2904678fa6f3160bd550ef5ba1cfa07
Do Chi Hung <dochihungf@gmail.com>
2024-09-08 15:31:53 +0700

    Feat: Add exception handlers identity services

--- Src/Open.Identity/Extensions/DependencyInjection.cs
+++ Src/Open.Identity/Extensions/DependencyInjection.cs
@@ -1,6 +1,24 @@
-﻿namespace Open.Identity.Extensions;
+﻿using Open.Core.Exceptions.ExceptionHandlers;
+
+namespace Open.Identity.Extensions;
 
 public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddWebServices(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    private static IServiceCollection AddExceptionHandlers(this IServiceCollection services)
+    {
+        services.AddExceptionHandler<ValidationExceptionHandler>();
+        services.AddExceptionHandler<_3pAttackExceptionHandler>();
+        services.AddExceptionHandler<BadRequestExceptionHandler>();
+        services.AddExceptionHandler<CatchableExceptionHandler>();
+        services.AddExceptionHandler<NotFoundExceptionHandler>();
+        services.AddExceptionHandler<SqlInjectionExceptionHandler>();
+        services.AddExceptionHandler<ForbiddenExceptionHandler>();
+        services.AddExceptionHandler<GlobalExceptionHandler>();
+        services.AddProblemDetails();
+    }
 }
